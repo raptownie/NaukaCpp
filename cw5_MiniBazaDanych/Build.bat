9ae7ebe9f3b5911c99696ae3a71ca14f68fe054c
raptownie <75669522+raptownie@users.noreply.github.com>
2022-11-28 17:51:07 +0100

    Added error handler if \obj dir not exist

--- cw5_MiniBazaDanych/Build.bat
+++ cw5_MiniBazaDanych/Build.bat
@@ -1,5 +1,6 @@
 @cls
 @SET PATH+="D:\etools\IDE\x86\mingw\GCC4.4.0\bin"
+@if not exist "obj\" mkdir obj\
 
 @echo **************************** [COMPILING] ****************************
 @mingw32-make.exe
